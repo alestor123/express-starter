7bd9be54877520b768e7e911539521515d7335f1
alestor123 <alestoraldous@gmail.com>
2020-08-29 10:41:12 +0530

    changed port listner

--- App.js
+++ App.js
@@ -2,8 +2,9 @@ var express = require('express'),
     port  = process.env.PORT || 3000;
 app = express();
 
-app.listen(port, () => console.log(`server running at ${port}`))
 
 app.get('/', (req, res) => {
 res.json({hello:"world"})
 })
+
+app.listen(port, () => console.log(`server running at ${port}`))
